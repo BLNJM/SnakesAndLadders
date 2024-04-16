8137459794774de900014f1485fce18e93496aa7
Lawrence Najim <najim.lawrence@gmail.com>
2024-04-16 13:05:28 +1200

    Create GUI window

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import tkinter as tk
+
+
+class GUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Snakes and Ladders")
+        self.canvas = tk.Canvas(self.root, width=500, height=500)
+        self.canvas.pack()
+        
+
 if __name__ == "__main__":
-    print("Hello world")
+    root = tk.Tk()
+    game = GUI(root)
+    root.mainloop()
