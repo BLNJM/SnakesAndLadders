5d0db8402f739b6830c786bfcc403de5a44e82db
Lawrence Najim <najim.lawrence@gmail.com>
2024-04-16 15:02:20 +1200

    Number of players user input code
    Also handles exceptions

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ class GUI:
         
 
 if __name__ == "__main__":
+
+    # ask the user for the player count
+    while True:
+        try:
+            num_players = int(input("Enter the player count: "))
+
+            # only accept answer if it is valid (an integer >= 2)
+            if num_players < 2:
+                print("At least 2 players are required")
+            if isinstance(num_players, int) and num_players > 1:
+                break
+        except ValueError:
+            # only accept answer if it is an integer
+            print("Invalid response, please enter the number of players")
+
     root = tk.Tk()
     game = GUI(root)
     root.mainloop()
